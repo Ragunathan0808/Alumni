4c98ecc5ab4dbea6d7918b1409c78bb880a6af77
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-12-08 22:54:05 +0530

    update user model

--- backend-rest/domain/models/user.model.go
+++ backend-rest/domain/models/user.model.go
@@ -1,30 +1,26 @@
 package models
 
-import (
-	"time"
-
-	"gorm.io/gorm"
-)
-
 type Country struct {
 	ID   uint   `json:"id" gorm:"primaryKey;autoIncrement"`
 	Name string `json:"name"`
 }
 
 type User struct {
-	ID        uint           `json:"id" gorm:"primaryKey;autoIncrement"`
-	CreatedAt time.Time      `json:"createdAt"`
-	UpdatedAt time.Time      `json:"updatedAt"`
-	DeletedAt gorm.DeletedAt `json:"deletedAt"`
+	ID        uint   `json:"id" gorm:"primaryKey;autoIncrement"`
+	CreatedAt uint64 `json:"createdAt" gorm:"autoCreateTime"`
+	UpdatedAt uint64 `json:"updatedAt" gorm:"autoUpdateTime"`
 
-	MailID       string `json:"mailID" gorm:"not null"`
+	MailID       string `json:"mailID" gorm:"unique;not null"`
 	PasswordHash string `json:"passwordHash" gorm:"not null"`
 	RandomHash   string `json:"randomHash" gorm:"not null"`
+	Verified     bool   `json:"verified" gorm:"default:false"`
+	Active       bool   `json:"active" gorm:"default:false"`
+	Type         string `json:"type" gorm:"default:user"`
 
 	Name        string  `json:"name" gorm:"not null"`
-	StudID      string  `json:"studID" gorm:"not null"`
-	Designation string  `json:"designation"`
-	Batch       int     `json:"batch"`
+	StudID      string  `json:"studID" gorm:"unique;not null"`
+	Designation string  `json:"designation" gorm:"index"`
+	Batch       uint    `json:"batch" gorm:"index"`
 	Address     string  `json:"address"`
 	CountryID   Country `json:"countryID" gorm:"foreignKey:ID"`
 	MobileNo    string  `json:"mobileNo"`
