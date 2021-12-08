42fc42d0ca2067289c0fc751644ba5cbdcb54293
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-12-08 22:59:04 +0530

    update user repo

--- backend-rest/domain/models/user.go
+++ backend-rest/domain/models/user.go
@@ -1,14 +1,14 @@
 package models
 
 type UserRepo interface {
-	Create(*User) (error, *User)
+	Create(*User) (*User, error)
 
 	DeleteUserByID(uint) error
 
-	FindByID(uint) (error, *User)
-	FindByMail(uint) (error, *User)
-	FindByDesignation(uint) (error, []*User)
-	FindByBatch(uint) (error, []*User)
+	FindByID(uint) (*User, error)
+	FindByMail(uint) (*User, error)
+	FindByDesignation(uint) ([]*User, error)
+	FindByBatch(uint) ([]*User, error)
 
 	UpdatePassword(uint, string) error
 	UpdateVerify(uint, bool) error
