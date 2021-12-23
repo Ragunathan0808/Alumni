cbf28aa75bf04a55f7b1c9d6217762d67563f08d
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-12-23 11:39:49 +0530

    add user service logic

---
+++ backend-rest/services/user.service.go
@@ -0,0 +1,33 @@
+package service
+
+import "github.com/kabi175/alumini-app-backend/domain/models"
+
+type DefaultUserService struct {
+	ur models.UserRepo
+}
+
+func NewDefaultUserService(ur models.UserRepo) models.UserService {
+	return &DefaultUserService{
+		ur: ur,
+	}
+}
+
+func (as *DefaultUserService) GetByID(userID uint) (*models.User, error) {
+	user, err := as.ur.FindByID(userID)
+	return user, err
+}
+
+func (as *DefaultUserService) GetByEmail(email string) (*models.User, error) {
+	user, err := as.ur.FindByMail(email)
+	return user, err
+}
+
+func (as *DefaultUserService) GetByDesignation(designation string) ([]*models.User, error) {
+	users, err := as.ur.FindByDesignation(designation)
+	return users, err
+}
+
+func (as *DefaultUserService) GetByBatch(batch uint) ([]*models.User, error) {
+	users, err := as.ur.FindByBatch(batch)
+	return users, err
+}
