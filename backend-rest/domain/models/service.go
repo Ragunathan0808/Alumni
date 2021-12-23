3ea97af78ae554f6865001c98bf7291c76f199b0
kabi175 <kabilan.ec19@bitsathy.ac.in>
2021-12-23 11:38:56 +0530

    add user, email, otp service

--- backend-rest/domain/models/service.go
+++ backend-rest/domain/models/service.go
@@ -4,9 +4,27 @@ type AuthService interface {
 	LogIn(*User) (string, error)
 	SignUp(*User) error
 	RemoveAccount(*User) error
+
+	UpdateActive(*User, bool) error
 }
 
 type TokenService interface {
 	GenerateToken(*User) (string, error)
 	DecodeToken(string) (*User, error)
+}
+
+type UserService interface {
+	GetByID(uint) (*User, error)
+	GetByEmail(string) (*User, error)
+	GetByDesignation(string) ([]*User, error)
+	GetByBatch(uint) ([]*User, error)
+}
+
+type EmailService interface {
+	Send(string, string, string, string) error
+}
+
+type OTPService interface {
+	GenerateOTP(*User) error
+	VerifyOTP(string) error
 }
